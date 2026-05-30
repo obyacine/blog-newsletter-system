17f28c27ef39bfa836624b5e3b6fea158d483d00
Yacine OULD <obyacine73@gmail.com>
2026-05-31 00:44:26 +0200

    fixed admin.py

--- app/models/admin.py
+++ app/models/admin.py
@@ -7,11 +7,5 @@ class Admin(Base):
     id=Column(Integer,primary_key=True,index=True)
     name=Column(String,nullable=False)
     email=Column(String,unique=True , index=True)
-    hased_password=(String)
-
-
-
-
-
-
+    hashed_password=Column(String)
 
